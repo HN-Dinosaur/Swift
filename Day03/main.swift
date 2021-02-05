78c86d4899104ad73819d09cf7255bcf147e8d3e
Dinosaur <76586071+HN-Dinosaur@users.noreply.github.com>
2021-02-05 23:34:05 +0800

    Add files via upload

---
+++ Day03/main.swift
@@ -0,0 +1,56 @@
+//
+//  main.swift
+//  Day03
+//
+//  Created by LongDengYu on 2021/2/5.
+//
+
+import Foundation
+//数组
+/*
+var someInt = [Int]()
+someInt.append(1)
+print(someInt.count)
+someInt.append(2)
+print(someInt)
+someInt = []
+print(someInt)
+print(someInt.capacity)
+
+var doubleArray = Array(repeating: 0.0, count: 4)
+print(doubleArray)
+var doubleArray2 = Array(repeating: 2.1, count: 4)
+doubleArray += doubleArray2
+print(doubleArray)
+var arrayInt: [Int] = [1,2,3]
+print(arrayInt)
+var arrayString: [String] = ["ni","真","漂","亮"]
+print(arrayString)
+
+arrayInt
+*/
+//集合
+/*
+var letter = Set<Int>()
+letter.insert(10)
+letter.insert(20)
+letter.insert(50)
+letter.insert(10)
+print(letter.capacity)
+var f: Set = ["xixi","kiki"]
+print(f.contains("xix"))
+var k: Set = ["xix"]
+print(f.isSuperset(of: k))
+*/
+//字典
+//var dict = ["age":"18","name":"ming"]
+//print(dict)
+//
+//var dict2: [String:String] = ["kik":"18","xixi":"19"]
+//print(dict2)
+//if let oldValue = dict2.updateValue("20", forKey: "xixi"){
+//    print(oldValue)
+//}
+//print(dict2)
+//print(dict2.removeValue(forKey: "xixi")!)
+//print(dict2)

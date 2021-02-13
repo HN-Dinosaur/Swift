7cb18e11b3059f2168435c3f413bf79d2e17d4df
Dinosaur <76586071+HN-Dinosaur@users.noreply.github.com>
2021-02-13 10:26:31 +0800

    Add files via upload

---
+++ Day08/main.swift
@@ -0,0 +1,50 @@
+//
+//  main.swift
+//  Day09
+//
+//  Created by LongDengYu on 2021/2/13.
+//
+
+import Foundation
+
+//class Counter{
+//    var count = 0
+//    func increment(){
+//        count += 1
+//    }
+//    func increment(by amount: Int){
+//        count += amount
+//    }
+//    func reset() {
+//        count = 0
+//    }
+//}
+//let counter = Counter()
+//counter.increment()
+//print(counter.count)
+//counter.increment(by: 5)
+//print(counter.count)
+//counter.reset()
+//print(counter.count)
+
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(x deltaX: Double, y deltaY: Double){
+//        self = Point(x: x + deltaX, y: y + deltaY)
+//    }
+//}
+//var somePoint = Point(x: 1.0, y: 1.0)
+//somePoint.moveBy(x: 2.0, y: 3.0)
+//print("\(somePoint.x),\(somePoint.y)")
+
+//class Point {
+//    static var name = ""
+//    static func changeName(name: String){
+//        print("my name is\(name)")
+//    }
+//    func changeName2(){
+//        Point.name = ""
+//    }
+//}
+//Point.changeName(name: "Hei")
+

48040231ce5333cd58e2368b561bcb0f5fa6e77a
Dinosaur <76586071+HN-Dinosaur@users.noreply.github.com>
2021-02-14 23:54:07 +0800

    Add files via upload

---
+++ Day09/Initialization/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Day10
+//
+//  Created by LongDengYu on 2021/2/14.
+//
+
+import Foundation
+
+
+//Optional property
+
+//class SurveyQuestion{
+//    var text: String
+//    var response: String?
+//    init(text: String) {
+//        self.text = text
+//    }
+//    func ask() {
+//        print(text)
+//    }
+//}
+//
+//let cheeseQuestion = SurveyQuestion(text: "Do you like banana? ")
+//cheeseQuestion.ask()
+//cheeseQuestion.response = "Yes! I like it."
+//print(cheeseQuestion.response!)
+
+//
